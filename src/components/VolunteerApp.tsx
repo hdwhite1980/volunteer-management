21f0e48e563c7ff29b3e9cb86b39eaee4b016b1e
hdwhite1980 <hdwhite@ahts4me.com>
2025-06-11 20:59:41 -0400

    Fix query to use session id column instead of session_token

--- src/components/VolunteerApp.tsx
+++ src/components/VolunteerApp.tsx
@@ -1444,7 +1444,6 @@ const VolunteerApp = () => {
         event.target.value = '';
       }
     };
-  }
     return (
       <div className="min-h-screen bg-gray-50 p-6">
         <div className="max-w-4xl mx-auto">
